b2538726961808bf165b7a2ec7989f7e1da144e6
yunus <yunus@YUNUSHP>
2025-12-10 18:36:51 +0300

    Add project files.

---
+++ uygulama1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int sayi = rnd.Next(100);
+            bool oyun = false;
+            int tur = 0;
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int tahmin = Convert.ToInt32(Console.ReadLine());
+
+            while (!oyun)
+            {
+                tur++;
+                if (tahmin < sayi)
+                {
+                    Console.Write("Lütfen daha büyük bir sayı giriniz: ");
+                    tahmin = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (tahmin > sayi)
+                {
+                    Console.Write("Lütfen daha küçük bir sayı giriniz: ");
+                    tahmin = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("----------------------\nTebrikler oyunu kazandınız!\nToplam Tur Sayısı: " + tur);
+                    oyun = true;
+                }
+            }
+
+
+        }
+    }
+}
